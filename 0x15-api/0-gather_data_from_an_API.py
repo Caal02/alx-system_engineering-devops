c5329fe0f3d5d8bbbdd341148968b2598e196a54
caleb <caaleb02@gmial.com>
2023-02-08 21:45:49 +0000

    fix 0x15 from v14

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Returns to-do list information for a given employee ID."""
+"""Using RESET API Returns to-do list information for a given employee ID."""
 import requests
 import sys
 if __name__ == "__main__":
