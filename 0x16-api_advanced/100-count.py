5c7110ea7198173ab39e3c54174ffe48c681d8f2
Caleb <caaleb02@gmail.com>
2023-10-11 01:21:16 +0300

    100

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -6,6 +6,8 @@ from requests import get
 
 REDDIT = "https://www.reddit.com/"
 HEADERS = {'user-agent': 'my-app/0.0.1'}
+
+
 def count_words(subreddit, word_list, after="", word_dic={}):
     """
     Returns a list containing the titles of all hot articles for a
